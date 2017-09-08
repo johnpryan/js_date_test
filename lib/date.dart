bc6195aaecc5a7ea5ad2a94efcafc075c39d94d1
John Ryan <john.p.ryan4@gmail.com>
2017-09-07 17:09:03 -0700

    init

---
+++ lib/date.dart
@@ -0,0 +1,7 @@
+@JS()
+library date;
+
+import 'package:js/js.dart';
+
+external DateTime get date;
+external num get time;
---
+++ web/main.dart
@@ -0,0 +1,6 @@
+import 'package:js_date_test/date.dart';
+
+void main() {
+  print("date = ${date} is type ${date.runtimeType}");
+
+}
